670c456f37ccfe8421a3cd6b2f2c2624258959a0
ManzDev <JoseRomanHernandez@gmail.com>
2024-11-09 03:47:17 +0000

    Update dotfiles

--- install.sh
+++ install.sh
@@ -1,91 +1,55 @@
-#!/bin/bash
-
-# Script for prepare Debian
-# 2022 - Manz (manz.dev)
-
 export DEBIAN_FRONTEND=noninteractive
 
-# Update linux
-sudo apt-get autoremove -y
-sudo apt-get upgrade -y
+sudo apt update -y
+sudo apt install -y bash sudo wget git locales build-essential \
+  vim nano ccze jq less catimg tldr curl httpie man htop ncdu \
+  unzip zip bzip2 p7zip-full woff2 file imagemagick \
+  bat sl lolcat cmatrix ffmpeg \
+  libxcb-render0-dev libxcb-shape0-dev libxcb-xfixes0-dev
 
-# Essentials
-sudo apt-get install -y \
-  bash zsh zgen sudo wget git g++ make gnupg gnupg2 ca-certificates lsb-release \
-  vim nano libbrotli-dev cmake \
-  ccze jq less catimg nnn zoxide \
-  tldr curl httpie man googler ddgr neofetch \
-  htop ncdu icdiff \
-  unzip zip bzip2 p7zip-full \
-  locales locales-all \
-  bat exa \
-  sl lolcat cmatrix ffmpeg
+tldr -u
 
-# Fix batcat -> bat
-sudo ln -s /usr/bin/batcat /usr/local/bin/bat
+# docker image support
+export SHELL=/bin/bash
 
-# Oh-my-zsh
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
-echo source $HOME/.dotfiles/.zshrc >> ~/.zshrc
+# fuzzy finder
+git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+~/.fzf/install -all
 
-PNPM_HOME=$HOME/.local/share/pnpm
-PATH=$HOME/bin:/usr/local/bin:$HOME/.nvm:/usr/local/go/bin:$HOME/.deno/bin:$HOME/.cargo/bin:/usr/share/go/bin:$PNPM_HOME:$PATH
+# PNPM/Node
+curl -fsSL https://get.pnpm.io/install.sh | sh -
+source ~/.bashrc
+pnpm env use -g 20
+pnpm install -g svgo wipeclean ttf2woff emoj servor
 
-# Go install
-wget --quiet https://go.dev/dl/go1.18.1.linux-amd64.tar.gz
-tar -xvf go1.18.1.linux-amd64.tar.gz
-sudo mv go /usr/share
-rm go1.18.1.linux-amd64.tar.gz
+# Rust
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+source ~/.bashrc
+cargo install bat zoxide eza zellij hyperfine jless erdtree broot choose
+{
+  echo '--level 2'
+  echo '--icons'
+  echo '--human'
+  echo '-s size'
+} >> ~/.erdtreerc
+echo 'eval "$(zoxide init bash)"' >> ~/.bashrc
 
-# Go installations
+# Go
+wget -q "https://go.dev/dl/go1.23.2.linux-amd64.tar.gz" -O - | sudo tar -C /usr/local -xzf -
+echo 'export PATH=$PATH:/usr/local/go/bin:$HOME/go/bin' >> ~/.bashrc
 go install github.com/muesli/duf@latest
 go install github.com/charmbracelet/glow@latest
 
-# Deno install
-curl -fsSL https://deno.land/x/install/install.sh | sh
+# Oh my posh
+sudo apt install -y curl unzip fonts-powerline nano
+curl -s https://ohmyposh.dev/install.sh | bash -s
+{
+  echo 'export PATH=$PATH:$HOME/.local/bin'
+  echo 'eval "$(oh-my-posh init bash)"'
+} >> ~/.bashrc
 
-# Node/NPM/PNPM install
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
-curl -fsSL https://get.pnpm.io/install.sh | PNPM_VERSION=7.0.0-rc.7 sh -
-source $HOME/.nvm/nvm.sh
-nvm install --lts
-npm install -g svgo wipeclean ttf2woff
-
-# Rust install
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
-
-# Cargo installations
-sudo apt-get install libxcb-render0-dev libxcb-shape0-dev libxcb-xfixes0-dev -y
-cargo install jless
-cargo install zellij
-cargo install hyperfine
-
-# Docker install
-curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian \
-  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt-get update -y && sudo apt-get install docker-ce docker-ce-cli containerd.io -y
-sudo usermod -aG docker $USER
-sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-sudo chmod +x /usr/local/bin/docker-compose
-
-# Micro install
-curl https://getmic.ro | sudo bash
-sudo mv micro /usr/local/bin
-
-# Woff2
-git clone https://github.com/google/woff2.git
-cd woff2
-mkdir out
-cd out
-cmake ..
-make && sudo make install
-sudo cp woff2_* /usr/local/bin/
-cd ..
-cd ..
-rm -rf woff2
-
-# Change to ZSH
-sudo chsh -s /usr/bin/zsh
-
-zsh
+{
+  echo "alias ls='eza -l --level 1 --icons'"
+  echo "alias tree='erd'"
+  echo "alias cat='batcat'"
+} >> ~/.bashrc
